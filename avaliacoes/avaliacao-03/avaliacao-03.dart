abbb340df3ce212a9f74d59ef442035eee3a92b2
Larissa Trece <62892182+lstrece@users.noreply.github.com>
2023-03-30 22:13:18 -0300

    Add files via upload

---
+++ avaliacoes/avaliacao-03/avaliacao-03.dart
@@ -0,0 +1,36 @@
+void main(){
+  print("Questão 1:");
+  print("5 + 3 * 2 = ${5 + 3 * 2}\n"); //questão 1
+  
+  print("Questão 2:");
+  print("10 / 2 - 3 = ${10 / 2 - 3}\n"); //questão 2
+  
+  print("Questão 3:");
+  print("7 % 3 = ${7 % 3}\n"); //questão 3
+
+  print("Questão 4:");
+  int x = 1;
+  print("x += 5 = ${x += 5}\n"); //questão 4
+
+  print("Questão 5:");
+  int y = 2;
+  print("y *= 3 = ${y *= 3}\n"); //questão 5
+
+  print("Questão 6:");
+  print("!(2 < 5) || (3 > 1) = ${!(2 < 5) || (3 > 1)}\n"); //questão 6
+
+  print("Questão 7:");
+  var z;
+  print("z ?? 10 = ${z ?? 10}\n"); //questão 7
+  
+  print("Questão 8:");
+  print("2 + 2 == 4 && 3 + 3 == 6 = ${2 + 2 == 4 && 3 + 3 == 6}\n"); //questão 8
+  
+  print("Questão 9:");
+  print("5 < 3 || 4 > 2 && 6 != 6 = ${5 < 3 || 4 > 2 && 6 != 6}\n"); //questão 9
+
+  print("Questão 10:");
+  var a;
+  print("a ??= 10 = ${a ??= 10}"); //questão 10
+  
+}
